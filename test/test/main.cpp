b87a37baff92d20ffce5f9b9b7fb05c76efb0f8d
gwg <718324404@qq.com>
2019-04-28 13:50:04 +0800

    增加工程

---
+++ test/test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Gao,Wenguang on 2019/4/28.
+//  Copyright © 2019 test. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
